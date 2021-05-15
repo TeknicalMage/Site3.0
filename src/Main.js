40779211ae9861c3c609c2f1766d3a68db81d15a
TeknicaLmage <arcazjrodan@gmail.com>
2021-05-15 15:33:59 -0400

    React fun

--- src/Main.js
+++ src/Main.js
@@ -6,21 +6,47 @@ import Nav from "./Nav";
 import Apt from "./App"; 
 import projects from "./Project"
 import videos from "./Video"
+import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDom from 'react-dom'
 
 
-function App() {
-     return(
-       <Router>
-        <div className = "App">
-         <Nav /> 
-           <Switch>
-           <Route path ="/videos" component ={videos}/>
-           <Route path ="/projects" component ={projects}/>
-           </Switch>
-        </div>
-      </Router>
-     );
-}
+
+
+function routes() {
+  return(
+      <Router>
+            <div className = "App">
+             <Nav /> 
+               <Switch>
+               <Route path ="/videos" component ={videos}/>
+               <Route path ="/projects" component ={projects}/>
+               </Switch>
+            </div>
+          </Router>
+  );
+  
+
+  
+} 
+
+
+//class App extends React.Component{
+  
+//  render(){
+//    return(
+//      routes
+//    );
+
+
+
+//  }
+
+
+
+//}
+
+
 
 
 
